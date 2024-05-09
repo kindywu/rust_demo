c3041431868f0770a5c9f2940a7292aa576d7969
root <root@kindywu.>
2024-05-09 15:06:41 +0800

    feat: add some test cases

--- src/garden.rs
+++ src/garden.rs
@@ -3,3 +3,13 @@ pub mod vegetables;
 pub fn garden() {
     println!("I'am garden")
 }
+
+#[cfg(test)]
+mod tests {
+    use super::garden;
+
+    #[test]
+    fn test_garden() {
+        garden();
+    }
+}
--- src/garden/vegetables.rs
+++ src/garden/vegetables.rs
@@ -1,3 +1,12 @@
 pub fn vegetable() {
     println!("I'am vegetables")
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_vegetable() {
+        vegetable()
+    }
+}
