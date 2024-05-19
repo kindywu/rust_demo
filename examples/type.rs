4c906e43cc1f19b57bcdbace89f5bc51beea1a0f
root <root@kindywu.>
2024-05-19 14:34:02 +0800

    commit

--- examples/type.rs
+++ examples/type.rs
@@ -1,10 +1,17 @@
+use std::collections::HashMap;
+
 type List<T> = Vec<T>;
+type MapList<T> = HashMap<u32, List<T>>;
 
 fn main() {
     let mut list = List::new();
     for i in 1..=10 {
         list.push(i)
     }
-
-    println!("{list:?}")
+    println!("{list:?}");
+    let key = 1;
+    let mut map_list = MapList::new();
+    // map_list.entry(key).or_insert(list);
+    map_list.insert(key, list);
+    println!("{map_list:?}");
 }
