a10008b5acff9f9edd7b7483e025be85c64e347c
kindywu <kindywu@outlook.com>
2024-05-20 21:02:32 +0800

    commit

--- examples/tokio_fs.rs
+++ examples/tokio_fs.rs
@@ -16,14 +16,14 @@ async fn main() -> Result<()> {
 
 async fn create_file(dir_path: &str, file_name: &str, file_content: &str) -> Result<()> {
     create_dir_all(dir_path).await?;
-    let file_path = format!("{dir_path}/{}", file_name);
+    let file_path = format!("{dir_path}/{file_name}");
     let mut file = File::create(file_path).await?;
     file.write_all(file_content.as_bytes()).await?;
     Ok(())
 }
 
 async fn read_file(dir_path: &str, file_name: &str) -> Result<String> {
-    let file_path = format!("{dir_path}/{}", file_name);
+    let file_path = format!("{dir_path}/{file_name}");
     let mut file = File::open(file_path).await?;
     let mut contents = String::new();
     file.read_to_string(&mut contents).await?;
