4737e25c1261e19ea3d49427b0a92d2e1780f2f7
kindywu <kindywu@outlook.com>
2024-05-22 15:59:48 +0800

    commit

--- examples/lifetime.rs
+++ examples/lifetime.rs
@@ -1,15 +1,23 @@
-struct Person<'a> {
+struct Person<'a, 'b> {
     name: &'a str,
+    desc: &'b str,
     age: u32,
 }
 
-fn describe<'a>(person: &'a Person<'a>) {
-    println!("Name: {}, Age: {}", person.name, person.age);
+fn describe<'a, 'b>(person: &Person<'a, 'b>) {
+    println!(
+        "Name: {}, Desc: {}, Age: {}",
+        person.name, person.desc, person.age
+    );
 }
 
 fn main() {
     let name = "Alice";
-    let person = Person { name, age: 30 };
+    let person = Person {
+        name,
+        desc: "Good Girl",
+        age: 30,
+    };
 
     describe(&person);
 }
